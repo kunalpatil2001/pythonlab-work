0a735ad9fc0335d2fceb32654668fa542c188585
kunalpatil2001 <92154752+kunalpatil2001@users.noreply.github.com>
2022-04-01 16:08:19 +0530

    Create all data type.py

---
+++ all data type.py
@@ -0,0 +1,24 @@
+
+a= 2
+print(a,type(2))
+print()
+a1= 5.2
+print(a1,type(a1))
+print()
+a2= 1+2j
+print(a2,type(a2))
+print()
+a3= "kunal"
+print(a3,type(a3))
+print()
+lst= [1,'kunal', -2, 3.7]
+print(lst, type(lst))
+print()
+tup= (1, 2, -3, 9.4, "kunal")
+print(tup,type(tup))
+print()
+d= {1:"kunal",2:"mangesh",3:"abhinav",4:"ravi",5:"ajay",6:"vickey"}
+print(d,type(d))
+print()
+s= {1, 2, 8.4, -4, "kunal", "patil"}
+print(s,type(s))
